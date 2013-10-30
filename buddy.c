4c0d211996e45331e980bb2b7c49667100a858c6
Yuxin David Huang <dhuang@ginneko.local>
2013-10-30 19:59:17 -0400

    added gitignore file

--- buddy.c
+++ buddy.c
@@ -12,9 +12,10 @@
 #endif
 
 // function declarations
-void *malloc(size_t);
+void* malloc(size_t);
 void free(void *);
 void dump_memory_map(void);
+int raise_power(int);
 
 
 const int HEAPSIZE = (1*1024*1024); // 1 MB
@@ -22,10 +23,11 @@ const int MINIMUM_ALLOC = sizeof(int) * 2;
 
 // global file-scope variables for keeping track
 // of the beginning of the heap.
-void *heap_begin = NULL;
+void* heap_begin = NULL;
+void* freelist = NULL;
 
 
-void *malloc(size_t request_size) {
+void* malloc(size_t request_size) {
 
     // if heap_begin is NULL, then this must be the first
     // time that malloc has been called.  ask for a new
@@ -33,17 +35,37 @@ void *malloc(size_t request_size) {
     // the heap begin pointer.
     if (!heap_begin) {
         heap_begin = mmap(NULL, HEAPSIZE, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+        if (heap_begin == MAP_FAILED) {
+            fprintf(stderr, "malloc failed: %s\n", errno);
+        }
+        freelist = heap_begin;
+        *((uint32_t*)freelist + 0) = HEAP_SIZE;
+        *((uint32_t*)freelist + 1) = 0;
         atexit(dump_memory_map);
     }
+    
+    uint32_t alloc_size = raise_power(request_size + MINIMUM_ALLOC);
 
 
 }
 
-void free(void *memory_block) {
+void free(void* memory_block) {
+    if (!memory_block) { // memory_block == NULL
+        assert memory_block == NULL;
+        return;
+    }
 
 }
 
 void dump_memory_map(void) {
 
+}
+
+int raise_power(int num) {
+    int result = 1; // 2^0 = 1
+    while (result < num) {
+        result *= 2;
+    }
+    return result;
 }
 
